c3f13573a7919ef9ec9c267f56b1e8afe1288c0b
Tamilarasan <Tamilarasanpandiyan@gmail.com>
2024-04-23 23:39:29 +0530

    first c

---
+++ src/components/GetQuote.jsx
@@ -0,0 +1,9 @@
+
+
+export default function GetQuote() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/page/HomeMain.jsx
+++ src/page/HomeMain.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import gsap from "gsap";
